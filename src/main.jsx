c5173741fd67774b2869ea36a1ed68deabeffb89
@mak-master <sunasaramak1308@gmail.com>
2026-08-29 12:42:32 +0530

    Fix Cartcontext filename

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import { CartProvider } from "./context/CartContext";
+import { CartProvider } from "./context/Cartcontext";
 
 import "./index.css";
 
